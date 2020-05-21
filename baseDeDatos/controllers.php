b1f94cc2f76b43e6a0472a9427ae1925e1dcfa12
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-05-21 13:43:04 +0200

    base de datos

---
+++ baseDeDatos/controllers.php
@@ -0,0 +1,25 @@
+<?php
+
+$localhost = "localhost";
+$root = "root";
+$database = "proyecto_ejemplo";
+
+
+function conexion($localhost, $root = "root", $database = "proyecto_ejemplo") {
+    $conector = new mysqli($localhost, $root, $database);
+    return $conector;
+};
+
+function getPlatos($conector){
+    $resultado = $conector->query("SELECT * FROM 'platos'");
+    
+    return $resultado;
+
+}
+
+
+
+
+
+
+
---
+++ baseDeDatos/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'controllers.php';
+conexion($localhost, $root,"", $database);
+
+
+$platos = getPlatos($conector);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<?php 
+
+var_dump($platos) ;
+
+?>  
+</body>
+</html>
